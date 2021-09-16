c2703ab3a7666f9141b957de04cff45c1777e659
jaroldhakins <stivenhakins@gmail.com>
2021-09-16 19:53:59 +0000

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,4 +2,5 @@
 def square_matrix_simple(matrix=[]):
         cop_matrix = []
         for i in matrix:
-            
+                cop_matrix.append(list(map(lambda i: i**2, i)))
+        return cop_matrix
