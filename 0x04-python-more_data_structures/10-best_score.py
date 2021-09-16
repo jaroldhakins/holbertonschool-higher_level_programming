805058fffaea12e317b7a60f95abef7fd1ad8268
jaroldhakins <stivenhakins@gmail.com>
2021-09-16 21:31:54 +0000

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary == None or a_dictionary == {}:
         return None
     max_value = max(a_dictionary.values())
     for i, j in a_dictionary.items():
