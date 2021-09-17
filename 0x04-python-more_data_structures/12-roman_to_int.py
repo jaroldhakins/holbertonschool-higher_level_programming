cbe04e188ab967e06906a2d80dccbf6704f8f8bd
jaroldhakins <stivenhakins@gmail.com>
2021-09-17 17:28:17 +0000

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) is not str:
-        return None
+        return 0
     Dict = {
         'I': 1,
         'V': 5,
