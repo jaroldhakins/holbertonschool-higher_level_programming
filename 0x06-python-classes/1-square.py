cf234432da712c0006e7882b1f8e6ba0a97488be
jaroldhakins <stivenhakins@gmail.com>
2021-09-28 23:29:38 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''class square with private instance size'''
+
+
+class Square:
+    '''class square with private instance size'''
+    def __init__(self, size):
+        self.__size = size
