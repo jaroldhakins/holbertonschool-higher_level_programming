9bbc53b8da68175246f92bd2e8f379b7d86e8f7f
jaroldhakins <stivenhakins@gmail.com>
2021-09-28 02:47:17 +0000

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         total = a / b
-        print("Inside result: {:.0f}".format(total))
+        print("Inside result: {:.1f}".format(total))
     except:
         total = None
         print("Inside result: {}".format(total))
