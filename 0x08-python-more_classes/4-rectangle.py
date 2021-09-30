1c726d388e73f89f9c61a04dc12abdeb7a6561e2
jaroldhakins <stivenhakins@gmail.com>
2021-09-30 02:42:28 +0000

    4-rectangle.py

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+'''create a new class'''
+
+
+class Rectangle:
+    '''initialize class'''
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width + self.__height) * 2
+
+    def __str__(self):
+        string = ""
+        x = self.__height
+        if self.__width != 0 and self.__height != 0:
+            for i in range(self.__height):
+                for j in range(self.__width):
+                    string = string + "#"
+                if i != x - 1:
+                    string = string + "\n"
+        return string
+
+    def __repr__(self):
+        string = "Rectangle(" + str(self.__width) + ","
+        string = string + str(self.__height) + ")"
+        return string
