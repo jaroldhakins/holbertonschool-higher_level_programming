2638472539e80bd8c5c67555031c7783f97bc4bc
jaroldhakins <stivenhakins@gmail.com>
2021-10-06 23:32:34 +0000

    4-print_square.py

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+Prints
+Square
+with this character "#"
+'''
+
+
+def print_square(size):
+    """
+    prints a square using this char "#"
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    elif type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
