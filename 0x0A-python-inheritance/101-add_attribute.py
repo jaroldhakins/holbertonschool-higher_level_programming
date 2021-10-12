a946c4104c58fbfde8e1e53b5e8b981ee414fe37
jaroldhakins <stivenhakins@gmail.com>
2021-10-12 04:27:11 +0000

    101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+'''
+adds a new attribute
+if it's possible
+'''
+
+
 def add_attribute(*args):
+    """init class
+    """
     if "main" in str(type(args[0])):
         setattr(args[0], args[1], args[2])
     else:
