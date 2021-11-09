455dbc7c9890ec8842a6a08d429092cc27ef629d
jaroldhakins <stivenhakins@gmail.com>
2021-11-09 03:41:20 +0000

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,101 +1,55 @@
 #!/usr/bin/python3
-"""New class, inherits from Base class"""
-
+''' First rectangle
+'''
 
 from models.base import Base
 
 
 class Rectangle(Base):
-    """initializing class"""
-    Rec_dict = {'id': 'id', 'width': '_Rectangle__width',
-                'height': '_Rectangle__height',
-                'x': '_Rectangle__x', 'y': '_Rectangle__y'}
+    '''class Rectangle inherits from base
+    '''
+    KV_dict = {'id': 'id', 'width': '_Rectangle__width',
+               'height': '_Rectangle__height',
+               'x': '_Rectangle__x', 'y': '_Rectangle__y'}
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        '''method __init__ Initialization a Rectangle
+        '''
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
 
-    @property
-    def width(self):
-        """getter to obtain width."""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """setter to modify width."""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """getter to obtain height."""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """setter to modify height."""
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
-
-    @property
-    def x(self):
-        """getter to obtain x."""
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        """setter to modify x."""
-        if type(value) is not int:
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
-
-    @property
-    def y(self):
-        """getter to obtain y."""
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        """setter to modify y."""
-        if type(value) is not int:
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = value
-
-    def area(self):
-        """returns the area of rectangle"""
-        return self.__width * self.__height
-
-    def display(self):
-        """prints rectangle with this character #"""
-        print("\n" * (self.__y), end="")
-        for i in range(self.__height):
-            print(" " * self.__x + "#" * self.__width)
+    def update(self, *args, **kwargs):
+        '''method update
+        '''
+        key_list = ['id', '_Rectangle__width', '_Rectangle__height',
+                    '_Rectangle__x', '_Rectangle__y']
+        KV_dict = {'id': 'id', 'width': '_Rectangle__width',
+                   'height': '_Rectangle__height',
+                   'x': '_Rectangle__x', 'y': '_Rectangle__y'}
+        for idx, el in enumerate(args):
+            self.__dict__[key_list[idx]] = el
+        if len(args) == 0:
+            for key, val in kwargs.items():
+                self.__dict__[KV_dict[key]] = val
 
     def __str__(self):
+        '''method __str__
+        '''
+
         return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
                 self.__y, self.__width, self.__height))
 
-    def update(self, *args, **kwargs):
-        key_list = ['id', '_Rectangle__width',  '_Rectangle__height',
-                    '_Rectangle__x', '_Rectangle__y']
-        Rec_dict = {'id': 'id', 'width': '_Rectangle__width',
-                    'height': '_Rectangle__height',
-                    'x': '_Rectangle__x', 'y': '_Rectangle__y'}
-        for i, v in enumerate(args):
-            self.__dict__[key_list[i]] = v
-        if len(args) == 0:
-            for key, val in kwargs.items():
-                self.__dict__[Rec_dict[key]] = val
+    def area(self):
+        '''public_method area of rectangle
+        '''
+        return self.__width * self.__height
+
+    def display(self):
+        '''public method display self prints in stdout #
+        '''
+        print("\n" * (self.__y), end="")
+        for i in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
