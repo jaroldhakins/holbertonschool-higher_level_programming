b4327cec76eb233fefb73741867075ffebebb97a
jaroldhakins <stivenhakins@gmail.com>
2021-11-09 16:39:34 +0000

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,6 +5,7 @@
 from models.base import Base
 
 
+
 class Rectangle(Base):
     '''class Rectangle inherits from base
     '''
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,5 +9,21 @@ class Square(Rectangle):
     """New class Square"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Initializing Square"""
+        self.size = size
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Getter that retrieves size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter that sets the value of size"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Method that returns the square string representation"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} " \
+               f"- {self.width}"
