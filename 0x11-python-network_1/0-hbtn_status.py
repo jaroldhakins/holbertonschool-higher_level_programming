34193692f1a54d658e822befe14effe49e72a4bf
jaroldhakins <stivenhakins@gmail.com>
2022-01-15 16:57:57 +0000

    README.md

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,6 +2,7 @@
 # Python script that fetches https://intranet.hbtn.io/status
 import urllib.request
 
+
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
         content = res.read()
